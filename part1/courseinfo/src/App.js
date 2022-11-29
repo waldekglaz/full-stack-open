5d86f80cd4342187caa9677682da51f2e2e10929
Waldemar Głaz <w.glaz@cheil.com>
2022-11-29 13:30:50 +0100

    1.1: course information, step 1

---
+++ part1/courseinfo/src/App.js
@@ -0,0 +1,32 @@
+import Header from "./components/Header";
+import Content from "./components/Content";
+import Total from "./components/Total";
+function App() {
+  const course = "Half Stack application development";
+  const part1 = "Fundamentals of React";
+  const exercises1 = 10;
+  const part2 = "Using props to pass data";
+  const exercises2 = 7;
+  const part3 = "State of component";
+  const exercises3 = 14;
+  return (
+    <div>
+      <Header title={course} />
+      <Content
+        part1={part1}
+        exercises1={exercises1}
+        part2={part2}
+        exercises2={exercises2}
+        part3={part3}
+        exercises3={exercises3}
+      />
+      <Total
+        excersises1={exercises1}
+        excersises2={exercises2}
+        excersises3={exercises3}
+      />
+    </div>
+  );
+}
+
+export default App;
---
+++ part1/courseinfo/src/components/Total.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Total({ excersises1, excersises2, excersises3 }) {
+  return <p>Number of exercises {excersises1 + excersises2 + excersises3}</p>;
+}
+
+export default Total;
