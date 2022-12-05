fc14e5bd15e55da3556cb286c1152fc5ced3c8b6
Waldemar Głaz <w.glaz@cheil.com>
2022-12-05 15:54:33 +0100

    1.4: step4

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -3,20 +3,21 @@ import Content from "./components/Content";
 import Total from "./components/Total";
 function App() {
   const course = "Half Stack application development";
-  const part1 = { name: "Fundamentals of React", exercises: 10 };
-  const part2 = { name: "Using props to pass data", exercises: 7 };
+  const parts = [
+    { name: "Fundamentals of React", exercises: 10 },
+    { name: "Using props to pass data", exercises: 7 },
+    { name: "State of component", exercises: 14 },
+  ];
+  // const part1 = { name: "Fundamentals of React", exercises: 10 };
+  // const part2 = { name: "Using props to pass data", exercises: 7 };
 
-  const part3 = { name: "State of component", exercises: 14 };
+  // const part3 = { name: "State of component", exercises: 14 };
 
   return (
     <div>
       <Header title={course} />
-      <Content part={part1.name} exercises={part1.exercises} />
-      <Total
-        excersises1={part1.exercises}
-        excersises2={part2.exercises}
-        excersises3={part3.exercises}
-      />
+      <Content parts={parts} />
+      <Total parts={parts} />
     </div>
   );
 }
