06bfcfa3c62e85752012c598353c065146bfa085
Waldemar Głaz <w.glaz@cheil.com>
2022-12-05 17:23:06 +0100

    1.9: unicafe step4

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -2,16 +2,17 @@ import { useState } from "react";
 import Statistics from "./components/Statistics";
 
 function App() {
-  const [good, setGood] = useState(6);
-  const [neutral, setNeutral] = useState(2);
-  const [bad, setBad] = useState(1);
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
   return (
     <div>
       <h2>Give Feedback</h2>
       <button>Good</button>
       <button>Neutral</button>
       <button>Bad</button>
-      <h2>Statistics</h2>
+
       <Statistics good={good} bad={bad} neutral={neutral} />
     </div>
   );
--- part1/unicafe/src/components/Statistics.js
+++ part1/unicafe/src/components/Statistics.js
@@ -1,16 +1,22 @@
 import React from "react";
 
 function Statistics({ good, bad, neutral }) {
-  return (
-    <ul>
-      <li>good {good}</li>
-      <li>neutral {neutral}</li>
-      <li>bad {bad}</li>
-      <li>all {good + neutral + bad}</li>
-      <li>average {(good * 1 + bad * -1) / (good + neutral + bad)}</li>
-      <li>positive {good / (good + neutral + bad)} %</li>
-    </ul>
-  );
+  if (good > 0 || bad > 0 || neutral > 0) {
+    return (
+      <>
+        <h2>Statistics</h2>
+        <ul>
+          <li>good {good}</li>
+          <li>neutral {neutral}</li>
+          <li>bad {bad}</li>
+          <li>all {good + neutral + bad}</li>
+          <li>average {(good * 1 + bad * -1) / (good + neutral + bad)}</li>
+          <li>positive {good / (good + neutral + bad)} %</li>
+        </ul>
+      </>
+    );
+  }
+  return <p>No feedback given</p>;
 }
 
 export default Statistics;
