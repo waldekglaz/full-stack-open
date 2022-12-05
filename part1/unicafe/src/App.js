15468a454dc60a93e9e9d50cf8e8c86f0b30f77c
Waldemar Głaz <w.glaz@cheil.com>
2022-12-05 16:54:29 +0100

    1.7 unicafe step2

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,10 +1,22 @@
 import { useState } from "react";
 
 function App() {
-  const [good, setGood] = useState(0);
-  const [neutral, setNeutral] = useState(0);
-  const [bad, setBad] = useState(0);
-  return <div></div>;
+  const [good, setGood] = useState(6);
+  const [neutral, setNeutral] = useState(2);
+  const [bad, setBad] = useState(1);
+  return (
+    <div>
+      <h1>Statistics</h1>
+      <ul>
+        <li>good {good}</li>
+        <li>neutral {neutral}</li>
+        <li>bad {bad}</li>
+        <li>all {good + neutral + bad}</li>
+        <li>average {(good * 1 + bad * -1) / (good + neutral + bad)}</li>
+        <li>positive {good / (good + neutral + bad)} %</li>
+      </ul>
+    </div>
+  );
 }
 
 export default App;
