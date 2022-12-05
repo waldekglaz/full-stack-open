7e60025a29ff3f0f4cdd925a0cff468932941a91
Waldemar Głaz <w.glaz@cheil.com>
2022-12-05 16:36:17 +0100

    1.6 step1

---
+++ part1/unicafe/src/App.js
@@ -0,0 +1,10 @@
+import { useState } from "react";
+
+function App() {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+  return <div></div>;
+}
+
+export default App;
