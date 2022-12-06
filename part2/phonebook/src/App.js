1e25ed05212ca64de15ff5e984eaf1e2d55bb746
Waldemar Głaz <w.glaz@cheil.com>
2022-12-06 13:06:06 +0100

    2.6: The Phonebook Step1

---
+++ part2/phonebook/src/App.js
@@ -0,0 +1,37 @@
+import { useState } from "react";
+
+const App = () => {
+  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [newName, setNewName] = useState("");
+  const handleNameChange = (e) => {
+    setNewName(e.target.value);
+  };
+  const handleClick = (e) => {
+    e.preventDefault();
+    const newPerson = { name: newName };
+    setPersons(persons.concat(newPerson));
+    setNewName("");
+  };
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form>
+        <div>
+          name: <input value={newName} onChange={handleNameChange} />
+        </div>
+        <div>
+          <button type="submit" onClick={handleClick}>
+            add
+          </button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {persons.map((person) => (
+        <p>{person.name}</p>
+      ))}
+    </div>
+  );
+};
+
+export default App;
