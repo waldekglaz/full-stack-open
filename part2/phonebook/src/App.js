ad60b9b20a2fe8fbe9a062b23d8a9ec3567a5e67
Waldemar Głaz <w.glaz@cheil.com>
2022-12-06 16:13:43 +0100

    2.9*: The Phonebook Step4

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,10 +1,30 @@
 import { useState } from "react";
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [persons, setPersons] = useState([
+    { name: "Arto Hellas", number: "040-123456", id: 1 },
+    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
+    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
+    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
+  ]);
   const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
+  const [searchPerson, setSearchPerson] = useState("");
+  const filteredData = persons.filter((el) => {
+    //if no input the return the original
+    if (searchPerson === "") {
+      return el;
+    }
+    //return the item which contains the user input
+    else {
+      return el.name.toLowerCase().includes(searchPerson.toLowerCase());
+    }
+  });
   const handleNameChange = (e) => {
     setNewName(e.target.value);
+  };
+  const handleNumberChange = (e) => {
+    setNewNumber(e.target.value);
   };
   const handleClick = (e) => {
     e.preventDefault();
@@ -24,18 +44,27 @@ const App = () => {
       return;
     }
 
-    const newPerson = { name: newName };
+    const newPerson = { name: newName, number: newNumber };
     setPersons(persons.concat(newPerson));
     setNewName("");
-    console.log(persons);
+    setNewNumber("");
+  };
+  const handleSearch = (e) => {
+    setSearchPerson(e.target.value);
   };
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        search <input onChange={handleSearch} value={searchPerson} />
+      </div>
       <form>
         <div>
           name: <input value={newName} onChange={handleNameChange} />
+        </div>
+        <div>
+          number: <input value={newNumber} onChange={handleNumberChange} />
         </div>
         <div>
           <button type="submit" onClick={handleClick}>
@@ -44,8 +73,8 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map((person) => (
-        <p key={person.name}>{person.name}</p>
+      {filteredData.map((person) => (
+        <p key={person.name}>{`${person.name}: ${person.number}`}</p>
       ))}
     </div>
   );
